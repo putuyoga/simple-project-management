10bafce9df0fdea914787b8b759c0072a4cc2982
putuyoga <putuyoga@gmail.com>
2014-05-27 08:22:52 +0700

    isi menu

---
+++ application/views/menu/2.php
@@ -0,0 +1,5 @@
+	<ul>
+		<li class="header">PROJECT</li>
+		<li><a href="<?php echo base_url(); ?>index.php/projects">projects</a></li>
+		<li><a href="<?php echo base_url(); ?>index.php/tasks">tasks</a></li>
+	</ul>
