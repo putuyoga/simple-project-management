4727429fe200239ad8e002947060c01a522b614c
unknown <ekaariyansyah@ymail.com>
2014-05-29 13:57:13 +0700

    ubah link topbar pelanggan

--- application/controllers/pelanggan.php
+++ application/controllers/pelanggan.php
@@ -4,7 +4,7 @@ class pelanggan extends CI_Controller {
 	public function list_pelanggan(){
 		$data['user'] = $this->user->get_current_user();
 		$data['sidebar'] = $this->user->get_sidebar($data);
-		$data['topbar'] = $this->load->view('menu/admin/user', '', true);
+		$data['topbar'] = $this->load->view('menu/pelanggan', '', true);
 		
 		$dataTS['list'] = $this->sales_model->getAll();
 		$data['judul'] = "List Pelanggan";
@@ -21,6 +21,30 @@ class pelanggan extends CI_Controller {
 				$this->sales_model->del_by_id($id);
 				redirect('pelanggan/list_pelanggan');
 			
+		}
+	}
+	
+	public function pelanggan_baru()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		//$this->form_validation->set_rules('password', 'Password', 'required|matches[repassword]');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
+		
+		if ($this->form_validation->run() == FALSE)
+		{
+			$data['user'] = $this->user->get_current_user();
+			$data['sidebar'] = $this->user->get_sidebar($data);
+			$data['judul'] = "Tambah Pelanggan";
+			$this->load->view('header', $data);
+			$data['list_auth'] = get_list_job_title();
+			$this->load->view('sales/pelanggan_baru', $data);
+			$this->load->view('footer');
+		}
+		else
+		{
+			$this->sales_model->insert_pelanggan($this->input->post());
+			redirect('pelanggan/list_pelanggan/');
 		}
 	}
 	
