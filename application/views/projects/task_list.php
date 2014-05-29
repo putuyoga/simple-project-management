e6ce640e3dfd665855d6e1301ad4a0a63f348f3d
putuyoga <putuyoga@gmail.com>
2014-05-29 16:23:22 +0700

    file baru

---
+++ application/views/projects/task_list.php
@@ -0,0 +1,44 @@
+<p class="desc">Disini anda dapat melihat daftar project di sistem ini</p>
+<table>
+	<tr>
+		<th>
+			Nama
+		</th>
+		<th>
+			PM
+		</th>
+		<th>
+			Mulai
+		</th>
+		<th>
+			Selesai
+		</th>
+		<th>
+		</th>
+	</tr>
+	<?php foreach($list as $item) : ?>
+		<tr>
+			<td>
+				<a href="<?php echo base_url(); ?>/index.php/projects/detail/<?php echo $item['id']; ?>">
+					<?php echo $item['nama']; ?>
+				</a>
+			</td>
+			<td>
+				<?php echo $item['pm']; ?>
+			</td>
+			<td>
+				<?php echo date('d M Y',strtotime($item['tanggal_mulai'])); ?>
+			</td>
+			<td>
+				<?php echo date('d M Y',strtotime($item['tanggal_selesai'])); ?>
+			</td>
+			<td>
+				<a href="<?php echo base_url('index.php/projects/task/' . $item['id']); ?>">task</a>
+				&middot;
+				<a href="<?php echo base_url('index.php/projects/edit/' . $item['id']); ?>">edit</a>
+				&middot;
+				<a href="<?php echo base_url('index.php/projects/hapus/' . $item['id']); ?>">hapus</a>
+			</td>
+		</tr>
+	<?php endforeach; ?>
+</table>
