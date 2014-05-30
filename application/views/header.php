e31c44990b3688fe731380501dfda4e0dd5ab15d
putuyoga <putuyoga@gmail.com>
2014-05-30 23:20:31 +0700

    tambah css jquery-te dan simple slider

--- application/views/header.php
+++ application/views/header.php
@@ -3,6 +3,8 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/anytime.5.0.1-1403131246.min.css">
 	<link href="<?php echo base_url('css/select2.css'); ?>" rel="stylesheet"/>
+	<link href="<?php echo base_url('css/jquery-te-1.4.0.css'); ?>" rel="stylesheet"/>
+	<link href="<?php echo base_url('css/simple-slider.css'); ?>" rel="stylesheet"/>
 	<script src="<?php echo base_url('js/jquery-1.11.0.min.js'); ?>"></script>
 	
 </head>
