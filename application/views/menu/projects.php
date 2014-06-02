38b6ef87c1af330456c5d07b6f7ff046218bf013
putuyoga <putuyoga@gmail.com>
2014-06-02 09:13:44 +0700

    tambah meu baru

--- application/views/menu/projects.php
+++ application/views/menu/projects.php
@@ -1,3 +1,4 @@
 <ul class="menu_horizontal" style="margin-left: 50px;">
 	<li><a href="<?php echo base_url('index.php/projects/baru'); ?>">baru</a></li>
+	<li><a href="<?php echo base_url('index.php/projects/list_all'); ?>">list semua</a></li>
 </ul>
