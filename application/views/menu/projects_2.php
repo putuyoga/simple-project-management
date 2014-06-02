eaee540d1c85ec78d786604fa000bd295891ca73
putuyoga <putuyoga@gmail.com>
2014-06-02 09:12:25 +0700

    file baru

---
+++ application/views/menu/projects_2.php
@@ -0,0 +1,4 @@
+<ul class="menu_horizontal" style="margin-left: 50px;">
+	<li><a href="<?php echo base_url('index.php/projects/baru'); ?>">baru</a></li>
+	<li><a href="<?php echo base_url('index.php/projects'); ?>">list belum selesai</a></li>
+</ul>
---
+++ application/views/menu/tasks_2.php
@@ -0,0 +1,6 @@
+<ul class="menu_horizontal" style="margin-left: 50px;">
+<?php if($user != 1): ?>
+	<li><a href="<?php echo base_url('index.php/tasks/baru/'); ?>">baru</a></li>
+<?php endif; ?>
+	<li><a href="<?php echo base_url('index.php/tasks/'); ?>">list belum selesai</a></li>
+</ul>
